01e563f1540c100f1b64830d092b114595802e85
Eric <70622326+AWSOMEDUDE00000090@users.noreply.github.com>
2024-04-23 08:36:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
-# df = pd.read_csv("US_Accidents_March23.csv")
+df = pd.read_csv("")
 
 country = gpd.read_file("C:\\Users\\1109367\\idp-final-project\\gz_2010_us_040_00_5m.json")
 country = country[(country['NAME'] != 'District of Columbia') & (country['NAME'] != 'Puerto Rico')]
 
 country.plot()
 plt.savefig('america.jpg', dpi=600)
-
-
-
-
-
